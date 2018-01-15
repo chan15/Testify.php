561e60ad05ce9e5a50438ee7114db90f18afa0d9
Chan <chan15tw@gmail.com>
2018-01-15 15:16:38 +0800

    Change coding style to fit company's convention

--- lib/Testify/Testify.php
+++ lib/Testify/Testify.php
@@ -2,40 +2,27 @@
 
 namespace Testify;
 
-use \Testify\TestifyException;
-
 /**
- * Testify - a micro unit testing framework
+ * Testify - a micro unit testing framework.
  *
  * This is the main class of the framework. Use it like this:
  *
  * @version    0.4.1
+ *
  * @author     Martin Angelov
  * @author     Marc-Olivier Fiset
  * @author     Fabien Salathe
- * @link       marco
- * @throws     TestifyException
+ *
+ * @see       marco
+ *
+ * @throws TestifyException
+ *
  * @license    GPL
  */
-
-class Testify {
-
-    private $tests = array();
-    private $stack = array();
-    private $fileCache = array();
-    private $currentTestCase;
-    private $suiteTitle;
-    private $suiteResults;
-
-    private $before = null;
-    private $after = null;
-    private $beforeEach = null;
-    private $afterEach = null;
-    
-    private $customReporter = null;
-
+class Testify
+{
     /**
-     * As html report need google api(font), while not available in China, this is an option to surrend to gfw(great fire wall)
+     * As html report need google api(font), while not available in China, this is an option to surrend to gfw(great fire wall).
      *
      * @var bool
      */
@@ -46,7 +33,21 @@ class Testify {
      *
      * @var \StdClass
      */
-    public $data = null;
+    public $data;
+
+    private $tests = array();
+    private $stack = array();
+    private $fileCache = array();
+    private $currentTestCase;
+    private $suiteTitle;
+    private $suiteResults;
+
+    private $before;
+    private $after;
+    private $beforeEach;
+    private $afterEach;
+
+    private $customReporter;
 
     /**
      * The constructor.
@@ -56,14 +57,26 @@ class Testify {
     public function __construct($title)
     {
         $this->suiteTitle = $title;
-        $this->data = new \StdClass;
+        $this->data = new \StdClass();
         $this->suiteResults = array('pass' => 0, 'fail' => 0);
+    }
+
+    /**
+     * Alias for {@see run} method.
+     *
+     * @see Testify->run()
+     *
+     * @return $this
+     */
+    public function __invoke()
+    {
+        return $this->run();
     }
 
     /**
      * Add a test case.
      *
-     * @param string $name Title of the test case
+     * @param string    $name     Title of the test case
      * @param \function $testCase (optional) The test case as a callback
      *
      * @return $this
@@ -72,12 +85,13 @@ class Testify {
     {
         if (is_callable($name)) {
             $testCase = $name;
-            $name = "Test Case #" . (count($this->tests) + 1);
+            $name = 'Test Case #'.(count($this->tests) + 1);
         }
 
-        $this->affirmCallable($testCase, "test");
+        $this->affirmCallable($testCase, 'test');
 
-        $this->tests[] = array("name" => $name, "testCase" => $testCase);
+        $this->tests[] = array('name' => $name, 'testCase' => $testCase);
+
         return $this;
     }
 
@@ -88,7 +102,7 @@ class Testify {
      */
     public function before(\Closure $callback)
     {
-        $this->affirmCallable($callback, "before");
+        $this->affirmCallable($callback, 'before');
         $this->before = $callback;
     }
 
@@ -99,7 +113,7 @@ class Testify {
      */
     public function after(\Closure $callback)
     {
-        $this->affirmCallable($callback, "after");
+        $this->affirmCallable($callback, 'after');
         $this->after = $callback;
     }
 
@@ -110,7 +124,7 @@ class Testify {
      */
     public function beforeEach(\Closure $callback)
     {
-        $this->affirmCallable($callback, "beforeEach");
+        $this->affirmCallable($callback, 'beforeEach');
         $this->beforeEach = $callback;
     }
 
@@ -121,7 +135,7 @@ class Testify {
      */
     public function afterEach(\Closure $callback)
     {
-        $this->affirmCallable($callback, "afterEach");
+        $this->affirmCallable($callback, 'afterEach');
         $this->afterEach = $callback;
     }
 
@@ -141,7 +155,7 @@ class Testify {
             call_user_func_array($this->before, $arr);
         }
 
-        foreach($this->tests as $test) {
+        foreach ($this->tests as $test) {
             $this->currentTestCase = $test['name'];
 
             if (is_callable($this->beforeEach)) {
@@ -168,8 +182,9 @@ class Testify {
     /**
      * Alias for {@see assertTrue} method.
      *
-     * @param boolean $arg The result of a boolean expression
+     * @param bool   $arg     The result of a boolean expression
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
+     *
      * @see Testify->assertTrue()
      *
      * @return boolean
@@ -182,7 +197,7 @@ class Testify {
     /**
      * Passes if given a truthfull expression.
      *
-     * @param boolean $arg The result of a boolean expression
+     * @param bool   $arg     The result of a boolean expression
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
@@ -195,7 +210,7 @@ class Testify {
     /**
      * Passes if given a falsy expression.
      *
-     * @param boolean $arg The result of a boolean expression
+     * @param bool   $arg     The result of a boolean expression
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
@@ -208,8 +223,8 @@ class Testify {
     /**
      * Passes if $arg1 == $arg2.
      *
-     * @param mixed $arg1
-     * @param mixed $arg2
+     * @param mixed  $arg1
+     * @param mixed  $arg2
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
@@ -222,8 +237,8 @@ class Testify {
     /**
      * Passes if $arg1 != $arg2.
      *
-     * @param mixed $arg1
-     * @param mixed $arg2
+     * @param mixed  $arg1
+     * @param mixed  $arg2
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
@@ -236,8 +251,8 @@ class Testify {
     /**
      * Passes if $arg1 === $arg2.
      *
-     * @param mixed $arg1
-     * @param mixed $arg2
+     * @param mixed  $arg1
+     * @param mixed  $arg2
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
@@ -250,8 +265,8 @@ class Testify {
     /**
      * Passes if $arg1 !== $arg2.
      *
-     * @param mixed $arg1
-     * @param mixed $arg2
+     * @param mixed  $arg1
+     * @param mixed  $arg2
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
@@ -264,29 +279,29 @@ class Testify {
     /**
      * Passes if $arg is an element of $arr.
      *
-     * @param mixed $arg
-     * @param array $arr
+     * @param mixed  $arg
+     * @param array  $arr
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
      */
     public function assertInArray($arg, array $arr, $message = '')
     {
-        return $this->recordTest(in_array($arg, $arr), $message);
+        return $this->recordTest(in_array($arg, $arr, true), $message);
     }
 
     /**
      * Passes if $arg is not an element of $arr.
      *
-     * @param mixed $arg
-     * @param array $arr
+     * @param mixed  $arg
+     * @param array  $arr
      * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
      *
      * @return boolean
      */
     public function assertNotInArray($arg, array $arr, $message = '')
     {
-        return $this->recordTest(!in_array($arg, $arr), $message);
+        return $this->recordTest(!in_array($arg, $arr, true), $message);
     }
 
     /**
@@ -324,22 +339,54 @@ class Testify {
         $title = $this->suiteTitle;
         $suiteResults = $this->suiteResults;
         $cases = $this->stack;
-        
+
         if (is_callable($this->customReporter)) {
-            call_user_func($this->customReporter, $title, $suiteResults, $cases );
-        } else if (php_sapi_name() === 'cli') {
-            include dirname(__FILE__) . '/testify.report.cli.php';
+            call_user_func($this->customReporter, $title, $suiteResults, $cases);
+        } elseif (PHP_SAPI === 'cli') {
+            include __DIR__.'/testify.report.cli.php';
         } else {
-            include dirname(__FILE__) . '/testify.report.html.php';
+            include __DIR__.'/testify.report.html.php';
         }
 
         return $this;
     }
 
     /**
+     * Alias for {@see assertEquals}.
+     *
+     * @deprecated Not recommended, use {@see assertEquals}
+     *
+     * @param mixed  $arg1
+     * @param mixed  $arg2
+     * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
+     *
+     * @return boolean
+     */
+    public function assertEqual($arg1, $arg2, $message = '')
+    {
+        return $this->assertSame($arg1, $arg2, $message);
+    }
+
+    /**
+     * Alias for {@see assertSame}.
+     *
+     * @deprecated Not recommended, use {@see assertSame}
+     *
+     * @param mixed  $arg1
+     * @param mixed  $arg2
+     * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
+     *
+     * @return boolean
+     */
+    public function assertIdentical($arg1, $arg2, $message = '')
+    {
+        return $this->recordTest($arg1 === $arg2, $message);
+    }
+
+    /**
      * A helper method for recording the results of the assertions in the internal stack.
      *
-     * @param boolean $pass If equals true, the test has passed, otherwise failed
+     * @param bool   $pass    If equals true, the test has passed, otherwise failed
      * @param string $message (optional) Custom message
      *
      * @return boolean
@@ -348,7 +395,6 @@ class Testify {
     {
         if (!array_key_exists($this->currentTestCase, $this->stack) ||
               !is_array($this->stack[$this->currentTestCase])) {
-
             $this->stack[$this->currentTestCase]['tests'] = array();
             $this->stack[$this->currentTestCase]['pass'] = 0;
             $this->stack[$this->currentTestCase]['fail'] = 0;
@@ -358,18 +404,18 @@ class Testify {
         $source = $this->getFileLine($bt[1]['file'], $bt[1]['line'] - 1);
         $bt[1]['file'] = basename($bt[1]['file']);
 
-        $result = $pass ? "pass" : "fail";
+        $result = $pass ? 'pass' : 'fail';
         $this->stack[$this->currentTestCase]['tests'][] = array(
-            "name"      => $message,
-            "type"      => $bt[1]['function'],
-            "result"    => $result,
-            "line"      => $bt[1]['line'],
-            "file"      => $bt[1]['file'],
-            "source"    => $source
+            'name' => $message,
+            'type' => $bt[1]['function'],
+            'result' => $result,
+            'line' => $bt[1]['line'],
+            'file' => $bt[1]['file'],
+            'source' => $source,
         );
 
-        $this->stack[$this->currentTestCase][$result]++;
-        $this->suiteResults[$result]++;
+        ++$this->stack[$this->currentTestCase][$result];
+        ++$this->suiteResults[$result];
 
         return $pass;
     }
@@ -378,7 +424,7 @@ class Testify {
      * Internal method for fetching a specific line of a text file. With caching.
      *
      * @param string $file The file name
-     * @param integer $line The line number to return
+     * @param int    $line The line number to return
      *
      * @return string
      */
@@ -394,8 +440,9 @@ class Testify {
     /**
      * Internal helper method for determine whether a variable is callable as a function.
      *
-     * @param mixed $callback The variable to check
-     * @param string $name Used for the error message text to indicate the name of the parent context
+     * @param mixed  $callback The variable to check
+     * @param string $name     Used for the error message text to indicate the name of the parent context
+     *
      * @throws TestifyException if callback argument is not a function
      */
     private function affirmCallable(&$callback, $name)
@@ -403,47 +450,5 @@ class Testify {
         if (!is_callable($callback)) {
             throw new TestifyException("$name(): is not a valid callback function!");
         }
-    }
-
-    /**
-     * Alias for {@see assertEquals}.
-     *
-     * @deprecated Not recommended, use {@see assertEquals}
-     * @param mixed $arg1
-     * @param mixed $arg2
-     * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
-     *
-     * @return boolean
-     */
-    public function assertEqual($arg1, $arg2, $message = '')
-    {
-        return $this->assertEquals($arg1, $arg2, $message);
-    }
-
-    /**
-     * Alias for {@see assertSame}.
-     *
-     * @deprecated Not recommended, use {@see assertSame}
-     * @param mixed $arg1
-     * @param mixed $arg2
-     * @param string $message (optional) Custom message. SHOULD be specified for easier debugging
-     *
-     * @return boolean
-     */
-    public function assertIdentical($arg1, $arg2, $message = '')
-    {
-        return $this->recordTest($arg1 === $arg2, $message);
-    }
-
-    /**
-     * Alias for {@see run} method.
-     *
-     * @see Testify->run()
-     *
-     * @return $this
-     */
-    public function __invoke()
-    {
-        return $this->run();
     }
 }
